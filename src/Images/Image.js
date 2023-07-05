71a1e08a2a6cd98cf2ac9634edd18adbda22aff9
juhikumarimodi6 <71226446+juhikumarimodi6@users.noreply.github.com>
2023-07-05 14:46:12 +0530

    Delete src/Images directory

--- src/Images/Image.js
+++
@@ -1,9 +0,0 @@
-import Banner1 from './banner1.jpg'
-import Banner2 from './banner2.jpg'
-import Banner3 from './banner3.jpg'
-import Banner4 from './banner4.jpg'
-import Banner5 from './banner5.jpg'
-import Banner6 from './banner6.jpg'
-import Banner7 from './banner7.jpg'
-
-export {Banner1, Banner2, Banner3, Banner4, Banner5, Banner6, Banner7}
